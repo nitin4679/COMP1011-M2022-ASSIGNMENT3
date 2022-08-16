b7e46dfca23e482a29d3d618f0222d99b8e7b20f
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-08-16 10:58:13 -0400

    Added APIManager Singleton class and movieIcon

--- src/main/java/ca/georgiancollege/comp1011m2022assignment3/Main.java
+++ src/main/java/ca/georgiancollege/comp1011m2022assignment3/Main.java
@@ -12,6 +12,7 @@ public class  Main extends Application {
     @Override
     public void start(Stage stage) throws IOException
     {
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("search-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Reading from OMDB API");
