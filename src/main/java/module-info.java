647dfcd108bcda57a016656bb574ca44ecb4e9d1
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-08-16 10:28:37 -0400

    added dependencies and modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module ca.georgiancollege.comp1011m2022assignment3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires com.google.gson;
+    requires java.net.http;
 
 
-    opens ca.georgiancollege.comp1011m2022assignment3 to javafx.fxml;
+    opens ca.georgiancollege.comp1011m2022assignment3 to javafx.fxml, com.google.gson;
     exports ca.georgiancollege.comp1011m2022assignment3;
 }
