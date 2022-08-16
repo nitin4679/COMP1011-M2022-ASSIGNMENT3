40dd54f8dffe7165f750df3e36ab0bdc63b2b05a
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-08-16 10:42:49 -0400

    added search-view.fxml and searchViewController

---
+++ src/main/java/ca/georgiancollege/comp1011m2022assignment3/SearchViewController.java
@@ -0,0 +1,19 @@
+package ca.georgiancollege.comp1011m2022assignment3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class SearchViewController {
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private ListView<?> resultsListView;
+
+    @FXML
+    private TextField searchTextField;
+
+}
